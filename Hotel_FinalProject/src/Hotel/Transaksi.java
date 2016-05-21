b766c15638bdc53c555d04b0f635900898dbed1b
Steven Willy Sanjaya <steven_willy@ymail.com>
2016-05-21 21:25:45 +0700

    update kelas Transaksi

--- Hotel_FinalProject/src/Hotel/Transaksi.java
+++ Hotel_FinalProject/src/Hotel/Transaksi.java
@@ -14,27 +14,47 @@ public class Transaksi implements DB {
     private Calendar batasCheckOut;
     private Calendar checkOut;
     private double totalHarga;
-    private static int count;
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-
-    public Transaksi(int hari,Pelanggan tamu, Kamar kamar) {
+    private double denda;
+    private static int count;   
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    public Transaksi(){
+        tamu = new Pelanggan();
+        kamar = new Kamar();
+        checkIn = Calendar.getInstance();
+        batasCheckOut = Calendar.getInstance();
+    }
+    public Transaksi(int hari, Pelanggan tamu, Kamar kamar) {
         kodeTransaksi = ++count;
         this.tamu = tamu;
         this.kamar = kamar;
         checkIn = Calendar.getInstance();
         batasCheckOut = Calendar.getInstance();
         batasCheckOut.add(Calendar.DATE, hari);
+        totalHarga = hari;
     }
 
-    private double checkDenda() {
-        double a = 0;
+    private void checkDenda() {
         checkOut = Calendar.getInstance();
         long selisih = checkOut.getTimeInMillis() - batasCheckOut.getTimeInMillis();
         long jam = TimeUnit.MILLISECONDS.toHours(selisih);
         if (jam > 0) {
-            a = jam * 30000;
+            denda = jam * 30000;
+        } else {
+            denda = 0;
         }
-        return a;
+
+    }
+
+    public double getTotalHarga() {
+        return totalHarga;
+    }
+
+    public double getDenda() {
+        return denda;
+    }
+    
+    public String toString() {
+        return String.format("%d\n%s\n%s\n", kodeTransaksi, String.valueOf(sdf.format(checkIn.getTime())), String.valueOf(sdf.format(batasCheckOut.getTime())));
     }
 
     public void savingData() throws SQLException {
@@ -45,19 +65,41 @@ public class Transaksi implements DB {
         String tanggalMasuk = String.valueOf(sdf.format(checkIn.getTime()));
         String nomorKamar = String.valueOf(kamar.getNoKamar());
         String batasOut = String.valueOf(sdf.format(batasCheckOut.getTime()));
-        String insert1 = String.format("insert into transaksi values (%s,%s,'%s')", kode,id,tanggalMasuk);
-        String insert2 = String.format("insert into detailTransaksi(kodeTransaksi,idPelanggan,nomorKamar,checkIn,batas) values(%s,%s,%s,'%s','%s')", 
-                kode,id,nomorKamar,tanggalMasuk,batasOut);
+        String insert1 = String.format("insert into transaksi values (%s,%s,'%s')", kode, id, tanggalMasuk);
+        String insert2 = String.format("insert into detailTransaksi(kodeTransaksi,idPelanggan,nomorKamar,checkIn,batas) values(%s,%s,%s,'%s','%s')",
+                kode, id, nomorKamar, tanggalMasuk, batasOut);
         stmt.executeUpdate(insert1);
         stmt.executeUpdate(insert2);
     }
 
-    public void CheckOut() {
-
+    public void CheckOut() throws SQLException {
+        checkDenda();
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotel?useSSL=false", "steven", "1111");
+        Statement stmt = con.createStatement();
+        totalHarga = (totalHarga * kamar.getHarga()) + denda;
+        String tanggalKeluar = String.valueOf(sdf.format(checkOut.getTime()));
+        String update = String.format("update detailTransaksi set checkOut = '%s', total = %s where kodeTransaksi = %s", tanggalKeluar, totalHarga, kodeTransaksi);
+        stmt.executeUpdate(update);
     }
 
     public void retrieveData(int id) throws SQLException {
-
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotel?useSSL=false", "steven", "1111");
+        Statement stmt = con.createStatement();
+        String select = String.format("select * from detailTransaksi where kodeTransaksi = %s", id);
+        ResultSet rset = stmt.executeQuery(select);
+        int ID=0,nomor=0;
+        while (rset.next()) {
+            kodeTransaksi = rset.getInt("kodeTransaksi");
+            ID = rset.getInt("idPelanggan");
+            nomor = rset.getInt("nomorKamar");
+            checkIn.setTime(rset.getTimestamp("checkIn"));
+            batasCheckOut.setTime(rset.getTimestamp("batas"));
+        }
+        long selisih = batasCheckOut.getTimeInMillis() - checkIn.getTimeInMillis();
+        long hari = TimeUnit.MILLISECONDS.toDays(selisih);
+        totalHarga = hari; 
+        kamar.retrieveData(nomor);
+        tamu.retrieveData(ID);
     }
 
 }
