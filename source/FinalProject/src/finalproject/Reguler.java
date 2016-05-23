e5aefd5bd69706204c381f47e3e46bee9cdf7eb9
Faza Abdi Perkasa <abdifaza@yahoo.com>
2016-05-23 22:58:40 +0700

    Membuat method untuk menggenerate nomor kamar secara otomatis

--- source/FinalProject/src/finalproject/Reguler.java
+++ source/FinalProject/src/finalproject/Reguler.java
@@ -5,14 +5,67 @@
  */
 package finalproject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Faza Abdi
  */
-public class Reguler extends Kamar{
-    
+public class Reguler extends Kamar {
+
+    private int nmrKmr;
+
     public Reguler(String Jenis) {
         super(Jenis);
     }
-    
+
+    public String getNmrKmr() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/Hotel", "faza", "aaaaa");
+        Statement stmt = conn.createStatement();
+        {
+            ResultSet hasil = stmt.executeQuery("select nmrKmr from datakamar where nmrKmr > 0");
+            int[] r = new int[50];
+            for (int i = 0; i < 50; i++) {
+                if (hasil.next()) {
+                    r[i] = hasil.getInt("nmrkmr");
+                }
+            }
+            for (int i = 0; i < r.length - 1; i++) {
+                for (int z = i + 1; z < r.length; z++) {
+                    if (r[z] < r[i]) {
+                        int temp;
+                        temp = r[z];
+                        r[z] = r[i];
+                        r[i] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < r.length - 1; i++) {
+                for (int z = i + 1; z < r.length; z++) {
+                    if (r[i] == 0 || r[z] == 0) {
+                        int temp;
+                        temp = r[z];
+                        r[z] = r[i];
+                        r[i] = temp;
+                    }
+                }
+            }
+            for (int i = 1; i <= 50; i++) {
+                if (r[i - 1] != 0) {
+                    if (r[i - 1] != i) {
+                        nmrKmr = i;
+                        break;
+                    }
+                } else {
+                    nmrKmr = i;
+                    break;
+                }
+            }
+        }
+        return String.valueOf(nmrKmr);
+    }
 }
