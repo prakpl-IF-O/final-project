ef1037f2bdfe401abf9db3e6f4f7a0847dc0bd37
Faza Abdi Perkasa <abdifaza@yahoo.com>
2016-05-23 23:00:36 +0700

    Membuat method untuk menggenerate nomor kamar secara otomatis

--- source/FinalProject/src/finalproject/Premium.java
+++ source/FinalProject/src/finalproject/Premium.java
@@ -5,14 +5,67 @@
  */
 package finalproject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Faza Abdi
  */
-public class Premium extends Kamar{
-    
+public class Premium extends Kamar {
+
+    private int nmrKmr;
+
     public Premium(String Jenis) {
         super(Jenis);
     }
-    
+
+    public String getNmrKmr() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/Hotel", "faza", "aaaaa");
+        Statement stmt = conn.createStatement();
+        {
+            ResultSet hasil = stmt.executeQuery("select nmrKmr from datakamar where nmrKmr > 50");
+            int[] p = new int[20];
+            for (int i = 0; i < 20; i++) {
+                if (hasil.next()) {
+                    p[i] = hasil.getInt("nmrkmr");
+                }
+            }
+            for (int i = 0; i < p.length - 1; i++) {
+                for (int z = i + 1; z < p.length; z++) {
+                    if (p[z] < p[i]) {
+                        int temp;
+                        temp = p[z];
+                        p[z] = p[i];
+                        p[i] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < p.length - 1; i++) {
+                for (int z = i + 1; z < p.length; z++) {
+                    if (p[i] == 0 || p[z] == 0) {
+                        int temp;
+                        temp = p[z];
+                        p[z] = p[i];
+                        p[i] = temp;
+                    }
+                }
+            }
+            for (int i = 51; i <= 70; i++) {
+                if (p[i - 51] != 0) {
+                    if (p[i - 51] != i) {
+                        nmrKmr = i;
+                        break;
+                    }
+                } else {
+                    nmrKmr = i;
+                    break;
+                }
+            }
+        }
+        return String.valueOf(nmrKmr);
+    }
 }
