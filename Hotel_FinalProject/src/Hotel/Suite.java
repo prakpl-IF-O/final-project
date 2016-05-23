0d8bd7d54717fc32523715b9d7e104a29318cd89
Satria Adhi Kharisma <SAKharisma@gmail.com>
2016-05-23 22:40:23 +0700

    Dokumentasi class Kamar dan beberapa kelas-kelas dari Kamar

--- Hotel_FinalProject/src/Hotel/Suite.java
+++ Hotel_FinalProject/src/Hotel/Suite.java
@@ -1,9 +1,14 @@
+//Package Hotel adalah package dari class Suite
 package Hotel;
 
+/*Deklarasi class Suite yang merupakan inheritance dari class Kamar
+  Class ini memiliki fungsi sebagai kelas dari kamar yang menentukan nomor kamar harga output kamar dengan kelas Suite */
 public class Suite extends Kamar {
 
+    /*Deklarasi constructor dengan melewatkan 1 parameter int noKamar 
+      Pada badan constructor terdapat 3 parameter yang merupakan inheritance dari constructor class Kamar */
     public Suite(int noKamar) {
         super(noKamar, 350000, "Suite");
     }
-    
+
 }
