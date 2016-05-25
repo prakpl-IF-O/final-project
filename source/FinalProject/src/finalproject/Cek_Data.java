0ee43d78cc4826beeb839c09e5300391a6ee8ebd
Faza Abdi Perkasa <abdifaza@yahoo.com>
2016-05-25 23:20:46 +0700

    Penambahan method pilah_data yang berfungsi untuk mengambil/mengecek data sesuai dengan id pelanggan

--- source/FinalProject/src/finalproject/Cek_Data.java
+++ source/FinalProject/src/finalproject/Cek_Data.java
@@ -5,6 +5,12 @@
  */
 package finalproject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Faza Abdi
@@ -12,8 +18,29 @@ package finalproject;
 public class Cek_Data {
 
     private String idd;
+    private String nmrKmr, jenis, id, nik, nama, ttl, ci, lama;
 
     public Cek_Data(String idd) {
         this.idd = idd;
     }
+
+    public void pilah_data() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/Hotel", "faza", "aaaaa");
+        Statement stmt = conn.createStatement();
+        {
+            ResultSet rset = stmt.executeQuery("select * from faza.datakamar where id=" + idd);
+            while (rset.next()) {
+                this.nmrKmr = rset.getString("nmrKmr");
+                this.jenis = rset.getString("jenis");
+                this.id = rset.getString("id");
+                this.nik = rset.getString("nik");
+                this.nama = rset.getString("nama");
+                this.ttl = rset.getString("ttl");
+                this.ci = rset.getString("ci");
+                this.lama = rset.getString("lama");
+
+            }
+        }
+    }
+
 }
