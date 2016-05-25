060ec8208209b390f9dd18f301a58338a458a7f8
Faza Abdi Perkasa <abdifaza@yahoo.com>
2016-05-25 22:50:53 +0700

    Pembuatan Kelas Check_In sekaligus menambahkan mutator NIK, LAMA, NAMA, TTL

---
+++ source/FinalProject/src/finalproject/Check_In.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject;
+
+/**
+ *
+ * @author Faza Abdi
+ */
+public class Check_In {
+
+    private String NIK, LAMA;
+    private String NAMA, TTL;
+
+    public void setNIK(String NIK) {
+        this.NIK = NIK;
+    }
+
+    public void setLAMA(String LAMA) {
+        this.LAMA = LAMA;
+    }
+
+    public void setNAMA(String NAMA) {
+        this.NAMA = NAMA;
+    }
+
+    public void setTTL(String TTL) {
+        this.TTL = TTL;
+    }    
+    
+}
