d763f8db12e4a16ad5a54482b9feaa2414a078a2
Faza Abdi Perkasa <abdifaza@yahoo.com>
2016-05-25 23:17:27 +0700

    Pembuatan kelas Cek_Data dan sekaligus menambahkan constructor berparameter String

---
+++ source/FinalProject/src/finalproject/Cek_Data.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject;
+
+/**
+ *
+ * @author Faza Abdi
+ */
+public class Cek_Data {
+
+    private String idd;
+
+    public Cek_Data(String idd) {
+        this.idd = idd;
+    }
+}
