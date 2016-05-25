9508800cbc2d0a0707cd16e6596f284e900dd73c
Faza Abdi Perkasa <abdifaza@yahoo.com>
2016-05-25 22:55:00 +0700

    Penambahan method proses, untuk memproses check in kamar apabila kamar masih tersedia

--- source/FinalProject/src/finalproject/Check_In.java
+++ source/FinalProject/src/finalproject/Check_In.java
@@ -5,6 +5,14 @@
  */
 package finalproject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
  *
  * @author Faza Abdi
@@ -28,6 +36,41 @@ public class Check_In {
 
     public void setTTL(String TTL) {
         this.TTL = TTL;
-    }    
-    
+    }
+
+    public void proses(String JENIS) {
+        try (Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/Hotel", "faza", "aaaaa");
+                Statement stmt = conn.createStatement();) {
+            Calendar cal = Calendar.getInstance();
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+            String CI = sdf.format(cal.getTime());
+            if (JENIS.equalsIgnoreCase("reguler")) {
+                Reguler r = new Reguler("Reguler");
+                if (!r.getNmrKmr().equalsIgnoreCase("0")) {
+                    int inputD = stmt.executeUpdate("INSERT INTO datakamar VALUES (" + r.getNmrKmr() + ", '" + JENIS + "', " + r.getIdPelanggan() + ", " + NIK + ", '" + NAMA + "', '" + TTL + "', '" + CI + "', " + LAMA + ")");
+                    int inputP = stmt.executeUpdate("INSERT INTO datapelanggan VALUES (" + NIK + ", '" + NAMA + "', '" + TTL + "', " + LAMA + ")");
+                }
+            } else if (JENIS.equalsIgnoreCase("premium")) {
+                Premium p = new Premium("Premium");
+                if (!p.getNmrKmr().equalsIgnoreCase("0")) {
+                    int inputD = stmt.executeUpdate("INSERT INTO datakamar VALUES (" + p.getNmrKmr() + ", '" + JENIS + "', " + p.getIdPelanggan() + ", " + NIK + ", '" + NAMA + "', '" + TTL + "', '" + CI + "', " + LAMA + ")");
+                    int inputP = stmt.executeUpdate("INSERT INTO datapelanggan VALUES (" + NIK + ", '" + NAMA + "', '" + TTL + "', " + LAMA + ")");
+                }
+            } else if (JENIS.equalsIgnoreCase("suite")) {
+                Suite s = new Suite("Suite");
+                if (!s.getNmrKmr().equalsIgnoreCase("0")) {
+                    int inputD = stmt.executeUpdate("INSERT INTO datakamar VALUES (" + s.getNmrKmr() + ", '" + JENIS + "', " + s.getIdPelanggan() + ", " + NIK + ", '" + NAMA + "', '" + TTL + "', '" + CI + "', " + LAMA + ")");
+                    int inputP = stmt.executeUpdate("INSERT INTO datapelanggan VALUES (" + NIK + ", '" + NAMA + "', '" + TTL + "', " + LAMA + ")");
+                }
+            } else if (JENIS.equalsIgnoreCase("vip")) {
+                Vip v = new Vip("Vip");
+                if (!v.getNmrKmr().equalsIgnoreCase("0")) {
+                    int inputD = stmt.executeUpdate("INSERT INTO datakamar VALUES (" + v.getNmrKmr() + ", '" + JENIS + "', " + v.getIdPelanggan() + ", " + NIK + ", '" + NAMA + "', '" + TTL + "', '" + CI + "', " + LAMA + ")");
+                    int inputP = stmt.executeUpdate("INSERT INTO datapelanggan VALUES (" + NIK + ", '" + NAMA + "', '" + TTL + "', " + LAMA + ")");
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
