6ecd49c52f045484e079a1bd5c4d2240e89edcea
Husein Abdulbar <abdulbarhusein@yahoo.com>
2016-05-25 08:17:18 +0700

    tambah class pendapatan akhir

---
+++ source/Akhir_Tugas/src/GUI/pendapatanakhir.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+/**
+ *
+ * @author Husein
+ */
+public class pendapatanakhir extends Checkin{
+    
+    public  String toString(){
+        return ("");
+    }
+    public int getPA(){
+        int pa=0;
+        pa+=super.total;
+        return pa;
+    }
+}
