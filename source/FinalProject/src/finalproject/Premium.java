f4c26c1b302623ad9d4a39f67b9db00bd3251fe6
Katherine Ivana Ruslim <katherineruslim@yahoo.co.id>
2016-05-26 20:50:06 +0700

    Membuat dokumentasi pada class Premium

--- source/FinalProject/src/finalproject/Premium.java
+++ source/FinalProject/src/finalproject/Premium.java
@@ -3,27 +3,43 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package finalproject;
+package finalproject; // lokasi pada package finalproject
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.Connection; // mengimport class Connection dalam package java.sql
+import java.sql.DriverManager; // mengimport class DriverManager dalam package java.sql
+import java.sql.ResultSet; // mengimport class ResulSet dalam package java.sql
+import java.sql.SQLException; // mengimport class SQLException dalam package java.sql
+import java.sql.Statement; // mengimport class Statement dalam package java.sql
 
 /**
  *
  * @author Faza Abdi
  */
+
+// Deklarasi kelas dengan nama Premium yang merupakan sub class dari kelas Kamar
 public class Premium extends Kamar {
 
+    // Deklarasi variabel nmrKmr berupa integer
+    // Deklarasi variabel harga sama dengan 200000 berupa double
     private int nmrKmr;
     private final double harga = 200000;
 
+    // Constructor Premium dengan parameter Jenis
+    // Memanggil constructor dari superclass 
     public Premium(String Jenis) {
         super(Jenis);
     }
 
+    // Method untuk mendapatkan nomor kamar secara otomatis
+    // Memasukkan syntax untuk mengakses data base
+    // Deklarasi statement
+    // Memasukkan statement berupa syntax SQL untuk mengambil data nomor kamar berdasarkan nmrKmr lebih dari 50 dan nmrKmr kurang dari 71
+    // Perulangan pertama untuk mengambil data nomor kamar dari database kedalam array p[]
+    // Perulangan kedua untuk mengurutkan array p[] dari data terkecil ke data terbesar
+    // Perulangan ketiga untuk menempatkan data nomor kamar yang kosong dibawah data nmrKmr yang telah memiliki nilai
+    // Perulangan keempat untuk mendapatkan nomor kamar apabila antar nomor kamar yang telah diurutkan memiliki kemungkinan untuk diisi dan 
+    // untuk mendapatkan nomor kamar setelah nomor kamar yang telah urut
+    // Mengembalikan nilai nmrKmr yang telah diparse kedalam String
     public String getNmrKmr() throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/Hotel", "faza", "aaaaa");
         Statement stmt = conn.createStatement();
@@ -70,6 +86,8 @@ public class Premium extends Kamar {
         return String.valueOf(nmrKmr);
     }
 
+    // Method yang mengimplementasikan method abstract dari kelas Kamar
+    // Mengembalikan nilai dari variabel harga
     @Override
     public double getHarga() {
         return harga;
