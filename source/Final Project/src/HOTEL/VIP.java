c181b2be0f6801811ad9802d633b231dec56a5c0
mfnaim <naimfattahp@gmail.com>
2016-05-26 19:36:01 +0700

    menambah dokumentasi

--- source/Final Project/src/HOTEL/VIP.java
+++ source/Final Project/src/HOTEL/VIP.java
@@ -1,3 +1,8 @@
+/*
+* sub class VIP inheritance dari Kamar
+* "VIP" adalah JenisKamar
+* 750000 adalah harga sewa dari kamar VIP selama satu hari
+*/
 package HOTEL;
 public class VIP extends Kamar{
     public VIP(){
