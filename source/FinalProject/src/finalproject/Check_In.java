d95b1b600bd4f47e6773ca8b04546f6971675843
Katherine Ivana Ruslim <katherineruslim@yahoo.co.id>
2016-05-26 19:07:00 +0700

    MMembuat dokumentasi pada class Check_In

--- source/FinalProject/src/finalproject/Check_In.java
+++ source/FinalProject/src/finalproject/Check_In.java
@@ -3,22 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package finalproject;
+package finalproject;// lokasi pada package finalproject
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.sql.Connection; // mengimport class Connection dalam package java.sql
+import java.sql.DriverManager; // mengimport class DriverManager dalam package java.sql
+import java.sql.ResultSet; // mengimport class ResulSet dalam package java.sql
+import java.sql.SQLException; // mengimport class SQLException dalam package java.sql
+import java.sql.Statement; // mengimport class Statement dalam package java.sql
+import java.text.SimpleDateFormat; // mengimport class SimpleDateFormat dalam package java.text
+import java.util.Calendar; // mengimport class Calendar dalam package java.util
 
 /**
  *
  * @author Faza Abdi
  */
 public class Check_In {
-
+    // Deklarasi variabel NIK, LAMA, NAMA, TTL, nmrKmr, id berupa String
     private String NIK, LAMA;
     private String NAMA, TTL;
     private String nmrKmr, id;
@@ -38,7 +38,15 @@ public class Check_In {
     public void setTTL(String TTL) {
         this.TTL = TTL;
     }
-
+    
+    // Method untuk menginputkan data check in kedalam database apabila kamar masih tersedia
+    // Memasukkan syntax untuk mengakses data base
+    // Deklarasi statement
+    // Deklarasi kelas Calendar dengan objek cal
+    // Instansiasi kelas SimpleDateFormat dengan objek sdf dengan format parameter "dd-MM-yyyy HH:mm"
+    // Mengambil tanggal saat method dieksekusi sesuai dengan format yang telah ditentukan kedalam variabel CI
+    // Melakukan proses seleksi kondisi berdasarkan variabel JENIS, kemudian menginputkan data checkin kedalam database apabila kamar masih tersedia
+    // Seluruh isi method tersebut berada didalam try catch untuk menangkap eksepsi atau kesalahan
     public void proses(String JENIS) {
         try (Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/Hotel", "faza", "aaaaa");
                 Statement stmt = conn.createStatement();) {
@@ -75,6 +83,14 @@ public class Check_In {
         }
     }
 
+    // Method untuk menampilkan ID yang digenerate secara otomatis
+    // Memasukkan syntax untuk mengakses data base
+    // Deklarasi statement
+    // Memasukkan statement berupa syntax SQL untuk mengambil data id berdasarkan NIK
+    // Assignment data yang didapatkan dari database kedalam variabel id
+    // Proses seleksi kondisi apakah id sama dengan null, apabila benar maka mengembalikan "ID Tidak Didapatkan" dan apabila salah maka mengembalikan 
+    // nilai dari variabel id 
+    
     public String getTampilID() throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/Hotel", "faza", "aaaaa");
         Statement stmt = conn.createStatement();
@@ -90,7 +106,14 @@ public class Check_In {
             return id;
         }
     }
-
+    
+    // Method untuk menampilkan nomor kamar yang digenerate secara otomatis
+    // Memasukkan syntax untuk mengakses data base
+    // Deklarasi statement
+    // Memasukkan statement berupa syntax SQL untuk mengambil data nomor kamar berdasarkan NIK
+    // Assignment data yang didapatkan dari database kedalam variabel nmrKmr
+    // Proses seleksi kondisi apakah nmrKmr sama dengan null, apabila benar maka mengembalikan "Kamar Penuh" dan apabila salah maka mengembalikan 
+    // nilai dari variabel nmrKmr
     public String getTampilNmrKmr() throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/Hotel", "faza", "aaaaa");
         Statement stmt = conn.createStatement();
