9c683382ee08f896a9991591236066f431fb47a8
mfnaim <naimfattahp@gmail.com>
2016-05-26 19:28:08 +0700

    menambah dokumentasi

--- source/Final Project/src/HOTEL/Reguler.java
+++ source/Final Project/src/HOTEL/Reguler.java
@@ -1,3 +1,8 @@
+/*
+* sub class Reguler inheritance dari class Kamar
+* "Reguler" adalah variabel JenisKamar
+* 100000 adalah harga sewa dari kamar Reguler selama satu hari
+*/
 package HOTEL;
 public class Reguler extends Kamar{
     public Reguler(){
