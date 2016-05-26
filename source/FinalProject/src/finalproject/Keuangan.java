6fb4e056466eab7276efca4d48b6e9ab85a37b6f
Katherine Ivana Ruslim <katherineruslim@yahoo.co.id>
2016-05-26 19:40:57 +0700

    Membuat dokumentasi pada class Keuangan

--- source/FinalProject/src/finalproject/Keuangan.java
+++ source/FinalProject/src/finalproject/Keuangan.java
@@ -3,23 +3,31 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package finalproject;
+package finalproject; // lokasi pada package finalproject
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
+import java.sql.Connection; // mengimport class Connection dalam package java.sql
+import java.sql.DriverManager; // mengimport class DriverManager dalam package java.sql
+import java.sql.ResultSet; // mengimport class ResulSet dalam package java.sql
+import java.sql.SQLException; // mengimport class SQLException dalam package java.sql
+import java.sql.Statement; // mengimport class Statement dalam package java.sql
 /**
  *
  * @author Faza Abdi
  */
 public class Keuangan {
 
+    // Deklarasi array of integer pendapatan yang memiliki jumlah indeks 1000
+    // Deklarasi variabel temp bertipe data double dan diberi nilai sama dengan 0 
     private int pendapatan[] = new int[1000];
     private double temp = 0;
 
+    // Method untuk mengecek pendapatan berdasarkan tanggal atau bulan atau tahun
+    // Memasukkan syntax untuk mengakses data base
+    // Deklarasi statement
+    // Memasukkan statement berupa syntax SQL untuk mengambil data pendapatan berdasarkan nilai dari variabel pil sama dengan nilai dari variabel tipe
+    // Assignment data pendapatan ke dalam array pendapatan
+    // Update nilai dari variabel k
+    // Proses perulangan untuk menjumlahkan seluruh nilai pada array pendapatan[] kedalam variabel temp 
     public void cek_keuangan(String pil, String tipe) throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/Hotel", "faza", "aaaaa");
         Statement stmt = conn.createStatement();
@@ -36,6 +44,8 @@ public class Keuangan {
         }
     }
 
+    // Method untuk mendapatkan total pendapatan
+    // Mengembalikan nilai dari variabel temp
     public double getTotalPendapatan() {
         return temp;
     }
