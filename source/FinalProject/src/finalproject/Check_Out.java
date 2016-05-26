3a521d7db36f0e3bb3cfdd7006957536d1552d29
Katherine Ivana Ruslim <katherineruslim@yahoo.co.id>
2016-05-26 19:11:55 +0700

    Membuat dokumentasi pada class Check_Out

--- source/FinalProject/src/finalproject/Check_Out.java
+++ source/FinalProject/src/finalproject/Check_Out.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package finalproject;
+package finalproject; // Lokasi pada package finalproject
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.Connection; // mengimport class Connection dalam package java.sql
+import java.sql.DriverManager; // mengimport class DriverManager dalam package java.sql
+import java.sql.SQLException; // mengimport class SQLException dalam package java.sql
+import java.sql.Statement; // mengimport class Statement dalam package java.sql
 
 /**
  *
@@ -16,6 +16,10 @@ import java.sql.Statement;
  */
 public class Check_Out {
 
+    // Method untuk menghapus data pelanggan yang menginap agar kamar yang dihuni tersedia kembali
+    // Memasukkan syntax untuk mengakses data base
+    // Deklarasi statement
+    // Memasukkan statement berupa syntax SQL untuk menghapus seluruh data berdasarkan NIK
     public void proses_check_out(String nik) throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/Hotel", "faza", "aaaaa");
         Statement stmt = conn.createStatement();
