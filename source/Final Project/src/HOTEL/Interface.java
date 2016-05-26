6e2564f627d3705e71915899cae0743fb0fd3502
mfnaim <naimfattahp@gmail.com>
2016-05-26 19:07:13 +0700

    Update Interface.java
    menambah dokumentasi

--- source/Final Project/src/HOTEL/Interface.java
+++ source/Final Project/src/HOTEL/Interface.java
@@ -1,3 +1,8 @@
+/* Class Interface 
+*
+* memanggil method Input dimana method Input adalah method untuk menerima input 
+* dari user yang kemudian disimpan ke dalam database.
+*/
 package HOTEL;
 public interface Interface {
     public void Input();
