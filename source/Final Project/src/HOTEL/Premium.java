aa83719be3244dd64d944f4843053177204dbbd1
mfnaim <naimfattahp@gmail.com>
2016-05-26 19:16:20 +0700

    menambah dokumentasi

--- source/Final Project/src/HOTEL/Premium.java
+++ source/Final Project/src/HOTEL/Premium.java
@@ -1,3 +1,7 @@
+/* 
+* sub Class Premium inheritance dari Kamar
+* 200000 yaitu harga sewa dari kamar Premium selama satu hari
+*/
 package HOTEL;
 public class Premium extends Kamar{
     public Premium (){
