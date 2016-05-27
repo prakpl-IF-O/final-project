85624d543d76cc966c078e3fa33d06d8f5167247
Muhammad Aditya Pramudita <adityapramudita23@gmail.com>
2016-05-27 17:39:00 +0700

    Menambahkan logic untuk mengeset harga masing kamar. dan diskon sesuai jumlah hari pelanggan menginap.

--- source/FinalProject/FinalProject/src/FinalProject/Transaksi.java
+++ source/FinalProject/FinalProject/src/FinalProject/Transaksi.java
@@ -61,4 +61,40 @@ public class Transaksi extends Kamar {
     public int getHarga() {
         return hargakamar;
     }
+
+    public void setHarga(String a) {
+        if ("Reguler".equals(a)) {
+            hargakamar = 100000;
+        } else if ("Premium".equals(a)) {
+            hargakamar = 200000;
+        } else if ("Suite".equals(a)) {
+            hargakamar = 350000;
+        } else if ("VIP".equals(a)) {
+            hargakamar = 750000;
+        }
+    }
+
+    public double getPembayaran() {
+        Kamar kmr = new Kamar();
+        if ((kmr.getJumlh() >= 0) && (kmr.getJumlh() <= 9)) {
+            Pembayaran = hari * hargakamar;
+        } else if ((kmr.getJumlh() > 9) && (kmr.getJumlh() <= 30)) {
+            Pembayaran = hari * hargakamar * 0.9;
+        } else if ((kmr.getJumlh() > 30) && (kmr.getJumlh() <= 60)) {
+            Pembayaran = hari * hargakamar * 0.85;
+        } else if (kmr.getJumlh() > 60) {
+            Pembayaran = hari * hargakamar * 0.75;
+        }
+        return Pembayaran;
+    }
+
+    public void menu() {
+        System.out.println("MENU : ");
+        System.out.println("1.Daftar Kelas dan Harga kamar");
+        System.out.println("2.Daftar Discount kelas kamar");
+        System.out.println("3.Daftar kamar kosong");
+        System.out.println("4.Transaksi pemesanan kamar");
+        System.out.println("5.Cetak transaksi");
+        System.out.println("6.Keluar ");
+    }
 }
