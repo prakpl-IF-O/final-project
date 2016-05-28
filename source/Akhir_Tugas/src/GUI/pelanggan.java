22af0a9a37ff899de451996e5eb8527d4c4236bb
andinadea <anidnadhea97@gmail.com>
2016-05-28 12:37:48 +0700

    Dokumentasi pelanggan

--- source/Akhir_Tugas/src/GUI/pelanggan.java
+++ source/Akhir_Tugas/src/GUI/pelanggan.java
@@ -4,21 +4,27 @@
  * and open the template in the editor.
  */
 package GUI;
+
 import java.sql.*;
+
 public class pelanggan {
-public int getjumlah(){
-    int a=0;
-    try { 
-            String url = "jdbc:derby://localhost:1527/hotel"; 
-            Connection conn = DriverManager.getConnection(url); 
-            Statement st = conn.createStatement(); 
-            ResultSet rs=st.executeQuery("SELECT * FROM pelanggan");
-            while(rs.next()){
+    /**
+     * Method getjumlah()
+     * @return nilai variabel a
+     */
+    public int getjumlah() {
+        int a = 0;
+        try {
+            String url = "jdbc:derby://localhost:1527/hotel";
+            Connection conn = DriverManager.getConnection(url);
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM pelanggan");
+            while (rs.next()) {
                 a++;
             }
-        } catch (Exception e) { 
-            System.err.println(e.getMessage()); 
-        } 
-    return a;
-}
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        return a;
+    }
 }
