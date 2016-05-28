016e567af8f20b2fa6bc20ed303342061b64348e
Salma Hanifah <salmahnfh98@gmail.com>
2016-05-28 19:12:44 +0700

    pemberian dokumentasi pada class Jenis

--- source/JavaApplication25/src/MainPackage/Jenis.java
+++ source/JavaApplication25/src/MainPackage/Jenis.java
@@ -1,7 +1,10 @@
 package MainPackage;
 
-
-
+/**abstract class dengan nama Jenis yang mengimplementasi class HotelMantap
+ * class Jenis diheritance oleh jenis kamar yang tersedia, 
+ * seperti premium, reguler, suite, dan vip agar class tersebut
+ * dapat mengoverride method yang telah dioverride class Jenis dari HotelMantap
+ */
 public abstract class Jenis implements HotelMantap {
     protected String tipe;
     protected boolean status;
