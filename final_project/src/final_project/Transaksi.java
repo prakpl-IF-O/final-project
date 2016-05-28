0490a157bb6dd22faa976e07bd40c81250a071b0
muhammadiqbalaulia <iqbalgendon21@gmail.com>
2016-05-28 18:03:37 +0700

    pembuatan class transaksi

---
+++ final_project/src/final_project/Transaksi.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package final_project;
+public class Transaksi  {
+
+    private String tglms, jms, tglk, jkel;
+    private int tothar, nokam;
+
+    public Transaksi(String tglms, String jms, String tglk, String jkel, int tothar, int nokam) {
+        this.tglms = tglms;
+        this.jms = jms;
+        this.tglk = tglk;
+        this.jkel = jkel;
+        this.tothar = tothar;
+        this.nokam = nokam;
+    }}
