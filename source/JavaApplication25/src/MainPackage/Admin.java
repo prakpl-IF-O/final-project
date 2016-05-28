605b923473c7126e3fd7257aa09ec8cf0a2b40ba
Salma Hanifah <salmahnfh98@gmail.com>
2016-05-28 09:11:40 +0700

    dokumentasi method pada class admin

--- source/JavaApplication25/src/MainPackage/Admin.java
+++ source/JavaApplication25/src/MainPackage/Admin.java
@@ -2,6 +2,13 @@ package MainPackage;
 public class Admin {
     private final static String[]admin={"mkharto","jerrykr","salmahnf"};
     private final static String[]paskey={"0906","",""};
+    
+    /**method login untuk login sebagai admin
+     * 
+     * @param a bertipe string sebagai username dari admin
+     * @param b bertipe string sebagai password
+     * @return nilai boolean apakah password dan username sesuai
+     */
     public boolean login(String a, String b){
         boolean c;
         if(a.equals(admin[0])&&b.equals(paskey[0]))c=true;
